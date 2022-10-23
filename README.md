56d6107c5552cd9c54287fbf718b432b1d87b548
Sam Cassidy <samuelcassidy1999@gmail.com>
2022-10-23 16:17:08 -0600

    My first program

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 # Make Change Project - Skill Distillery Week 1 Homework
 
 ### Description
-
+- A calculator that will dispense how much change is needed when making a purchase
+- Displays the least amount of bills needed to produce the outcome
+- Uses a carried variable throughout the math being done for maximum efficiency
+- Displays an error message when not enough money is entered to complete the purchase
+- Quickly solves the issue of how much change is needed for a complete transaction
 ### Lessons Learned
-
+- I do not know as much as I thought I did
+- My logic needs to be more concise
+  - I found myself jumping around to find which data types worked and didn't
+    - It caused much unnecessary confusion
+- I learned that I cannot skip any steps and sometimes the long way is the best way
+  - difficult to outline all of my thoughts in computer code
+- I learned I have a lot to learn
+- Printf statements are incredibly useful 
 ### Technologies Used
 - Java
 - Eclipse
 - Git/Github
 - Atom
+- Terminal
--- src/app/CashRegister.java
+++ src/app/CashRegister.java
@@ -1,10 +1,87 @@
 package app;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Please enter the price of an item you recently purchased: ");
+		double userP = sc.nextDouble();
+
+		System.out.println("Please enter the amount of cash you used: ");
+		double userC = sc.nextDouble();
+
+		double userChange = userC - userP;
+
+		double changeBack = userChange;
+
+		double oneV = 1;
+		double fiveV = 5;
+		double tenV = 10;
+		double twentyV = 20;
+
+		int one, five, ten, twenty;
+
+		double pennyV = .01;
+		double nickelV = .05;
+		double dimeV = .1;
+		double quarterV = 0.25;
+
+		double quarter, dime, nickel, penny;
+
+		if (userP > userC) {
+			System.out.println(" You have not paid the appropriate amount for this item.");
+		} else if (userC == userP) {
+			System.out.println("You have given exact change, no change needed");
+		} else {
+			System.out.println("The cashier owes you " + "$" + userChange);
+
+		}
+		changeBack = (double) (userChange);
+
+		twenty = (int) (changeBack / twentyV);
+		changeBack -= twenty * twentyV;
+
+		ten = (int) (changeBack / tenV);
+		changeBack -= ten * tenV;
+
+		five = (int) (changeBack / fiveV);
+		changeBack -= five * fiveV;
+
+		one = (int) (changeBack / oneV);
+		changeBack -= one * oneV;
+
+		quarter =  (int)(changeBack / quarterV);
+		changeBack -= (quarter * quarterV);
+
+		dime = (int)(changeBack / dimeV);
+		changeBack -= dime * dimeV;
+
+		nickel = (int)(changeBack / nickelV);
+		changeBack -= nickel * nickelV;
+
+		penny = (int)(changeBack / pennyV);
+		changeBack -= penny* pennyV;
+
+		System.out.printf("%d: Twenty dollar bills \n", twenty);
+
+		System.out.printf("%d: Ten dollar bills \n", ten);
+
+		System.out.printf("%d: Five dollar bills \n", five);
+
+		System.out.printf("%d: One dollar bills \n", one);
+
+		System.out.printf("%f: Quarters \n", quarter);
+		
+		System.out.printf("%f: Dimes \n", dime);
+		
+		System.out.printf("%f: Nickels \n", nickel);
+		
+		System.out.printf("%f: Pennies \n", penny);
+
+		sc.close();
 	}
 
 }
